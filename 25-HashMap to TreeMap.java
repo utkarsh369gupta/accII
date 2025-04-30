371c54e2c30a3fcabbb0445c9a6ff6fa6c376a98
Utkarsh369Gupta <115869655+utkarsh369gupta@users.noreply.github.com>
2025-04-30 22:15:14 +0530

    Create 25-HashMap to TreeMap.java

---
+++ 25-HashMap to TreeMap.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+
+public class Main{
+  public static void main(String[] args){
+    HashMap<Integer, Integer> map = new HashMap<>();
+    map.put(1, 1);
+    map.put(2, 2);
+    TreeMap<Integer, Integer> treemap = new TreeMap<>();
+
+   treemap.putAll(hashmap);
+  }
+}
